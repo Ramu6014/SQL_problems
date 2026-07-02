1fef0580eab54d3fde7a7cccbda8f614e92b8b39
Ramu_64 <ramukotti90@gmail.com>
2026-07-02 23:47:34 +0530

    added employeesEarningMoreThanTheirManagers problem

---
+++ 181. Employees Earning More Than Their Managers.sql
@@ -0,0 +1,8 @@
+//problem link: https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+# Write your MySQL query statement below
+select e.name as Employee
+from
+Employee e left join Employee m
+on e.managerId=m.id 
+where e.salary>m.salary
