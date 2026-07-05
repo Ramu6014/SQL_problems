86867a9b8650e3696846315228d1e6423451c289
Ramu_64 <ramukotti90@gmail.com>
2026-07-05 17:41:55 +0530

    added deleteDuplicateEmails problem

---
+++ 196. Delete Duplicate Emails.sql
@@ -0,0 +1,8 @@
+//problem link: https://leetcode.com/problems/delete-duplicate-emails/
+
+
+# Write your MySQL query statement below
+DELETE p1
+FROM Person as p1 JOIN Person as p2
+ON p1.email=p2.email
+WHERE p1.email=p2.email AND p1.id>p2.id;
