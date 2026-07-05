fac2db800961f7f224031406a4aea6a53ba6fd03
Ramu_64 <ramukotti90@gmail.com>
2026-07-05 17:39:41 +0530

    added duplicateEmails problem

---
+++ 182. Duplicate Emails.sql
@@ -0,0 +1,7 @@
+//problem link: https://leetcode.com/problems/duplicate-emails/
+
+# Write your MySQL query statement below
+SELECT email as Email
+FROM Person 
+GROUP BY (email)
+HAVING COUNT(email)>1;
